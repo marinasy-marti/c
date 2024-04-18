bcded8d63caa3c87a5047a854a208c5c5786239a
Marinasy <behambymarinasy@gmail.com>
2024-04-18 21:05:05 +0300

    data type

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
+
 #include <stdio.h>
 
 int main()
 {
-    printf("Hello World!");
+    //  the data type
+    /*
+    int             (4 bytes ) | %d for printing
+    double          (8 bytes ) | %lf for printing
+    float           (4 bytes ) | %f for printing
+    char            (1 bytes ) | %c for printing
+    */
+
+    double number = 2.34;
+    printf("%.2lf", number);
+
+    char letter = 'a';
+    printf("\n%c", letter);
     return 0;
-}
+};
