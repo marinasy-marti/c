d0979700e7991d3d222468b92db96601e08e3fa7
Marinasy <behambymarinasy@gmail.com>
2024-04-19 23:33:34 +0300

    comment

--- main.c
+++ main.c
@@ -4,27 +4,17 @@ int main()
 {
     int age;
 
-    printf("Enter your age: ");
-    scanf("%d", &age);
+    // printf("Enter your age: ");
+    // scanf("%d", &age);
 
-    printf("You are %d years old\n", age);
+    // printf("You are %d years old\n", age);
 
-    double number;
-    char abc;
-
-    printf("Enter double input: ");
-    scanf("%lf", &number);
-
-    printf("Enter char input: ");
-    scanf(" %c", &abc);
-
-    printf("Number: %lf\nChar: %c\n", number, abc);
-
-    printf("\nTwo type input ");
-    printf("\nEnter a value:");
-    scanf("%lf %c", &number, &abc);
-
-    printf("Number: %lf\nABC: %c\n", number, abc);
-
+    // this is a comment
+    /* this
+        is
+        a
+        multiline
+        comment
+    */
     return 0;
 }
